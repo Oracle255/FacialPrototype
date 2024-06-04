975f4f65991262c3026d10871548fd6cbb896029
oracle255 <ahmadfarikhb@gmail.com>
2024-06-04 15:35:53 +0700

    Ubah implementasi activity di main page menjadi fragment

--- app/src/main/java/com/capstone/prettyU/View/Activity/MainActivity.kt
+++ app/src/main/java/com/capstone/prettyU/View/Activity/MainActivity.kt
@@ -2,17 +2,25 @@ package com.capstone.prettyU.View.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.capstone.prettyU.BackEnd.Utilities.DevTestConfig.Companion.loremWithLength
 import com.capstone.prettyU.BackEnd.data.Item
 import com.capstone.prettyU.R
+import com.capstone.prettyU.View.Fragment.FaceScanFragment
+import com.capstone.prettyU.View.Fragment.MainPageFragment
 import com.capstone.prettyU.View.adapter.ItemData.TreatmentData
 import com.capstone.prettyU.View.adapter.RvAdapterHorizontal
 import com.capstone.prettyU.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var fragmentManager: FragmentManager
+    lateinit var bottomNav : BottomNavigationView
     //private lateinit var rvItem: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,24 +28,42 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        bottomNav = binding.bottomNav
 
-        val rvItem = binding.rvItem
-
-        rvItem.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false) // Set horizontal orientation
-
-        // TODO: (SET ONCLICK LISTENER) & Mock data json api
-        val dataList = listOf(
-            TreatmentData(R.drawable.ic_placeholder, "wdqwdqwdqwdqwdqw", "${loremWithLength(12)}"),
-            TreatmentData(R.drawable.ic_placeholder, "${loremWithLength(1)}", "${loremWithLength(12)}"),
-            TreatmentData(R.drawable.ic_placeholder, "${loremWithLength(1)}", "${loremWithLength(12)}")
-        )
-
-        val adapter = RvAdapterHorizontal(dataList)
-        rvItem.adapter = adapter
+        fragmentManager = supportFragmentManager
+        loadFragment(MainPageFragment())
+        navigationListener()
     }
 
     // TODO: set animasi
     private fun playAnimation() {
 
+    }
+
+
+    // Fragment switch
+
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainerView,fragment)
+        transaction.commit()
+    }
+
+    private fun navigationListener() {
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(MainPageFragment())
+                    true
+                }
+                R.id.message -> {
+                    loadFragment(FaceScanFragment())
+                    true
+                }
+
+                else -> {loadFragment(MainPageFragment())
+                    true}
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/capstone/prettyU/View/Fragment/MainPageFragment.kt
@@ -0,0 +1,92 @@
+package com.capstone.prettyU.View.Fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.capstone.prettyU.BackEnd.Utilities.DevTestConfig
+import com.capstone.prettyU.R
+import com.capstone.prettyU.View.adapter.ItemData.TreatmentData
+import com.capstone.prettyU.View.adapter.RvAdapterHorizontal
+import com.capstone.prettyU.databinding.FragmentMainPageBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainPageFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainPageFragment : Fragment() {
+
+    private var _binding: FragmentMainPageBinding? = null
+    private val binding get() = _binding!!
+
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainPageBinding.inflate(layoutInflater, container, false)
+        val view = binding.root
+        val rvItem = binding.rvItem
+
+        rvItem.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false) // Set horizontal orientation
+
+        // TODO: (SET ONCLICK LISTENER) & Mock data json api
+        val dataList = listOf(
+            TreatmentData(R.drawable.ic_placeholder, "wdqwdqwdqwdqwdqw", "${DevTestConfig.loremWithLength(12)}"),
+            TreatmentData(R.drawable.ic_placeholder, "${DevTestConfig.loremWithLength(1)}", "${DevTestConfig.loremWithLength(12)}"),
+            TreatmentData(R.drawable.ic_placeholder, "${DevTestConfig.loremWithLength(1)}", "${DevTestConfig.loremWithLength(12)}")
+        )
+
+        val adapter = RvAdapterHorizontal(dataList)
+        rvItem.adapter = adapter
+
+        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MainPageFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MainPageFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+    // TODO: set animasi
+    private fun playAnimation() {
+
+    }
+}
