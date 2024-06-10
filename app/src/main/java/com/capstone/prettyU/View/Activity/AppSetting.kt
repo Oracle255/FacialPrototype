d2efc56531e3a97a21d9746580b95392691fb8d5
ditta <a361d4kx4231@bangkit.academy>
2024-06-10 21:08:23 +0700

    add fragment profile and activity app setting

---
+++ app/src/main/java/com/capstone/prettyU/View/Activity/AppSetting.kt
@@ -0,0 +1,12 @@
+package com.capstone.prettyU.View.Activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.capstone.prettyU.R
+
+class AppSetting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_setting)
+    }
+}
